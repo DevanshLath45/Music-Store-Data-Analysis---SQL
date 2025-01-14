b0cf0874dd9790f306c99ac92cee521c71ff9681
Devansh Lath <devanshlath987654321@gmail.com>
2025-01-15 00:59:05 +0545

    Update SQLQuery.sql

--- SQLQuery.sql
+++ SQLQuery.sql
@@ -110,13 +110,13 @@ ORDER BY country_name;
 --Q11
 
 WITH new AS (
-			SELECT c.customer_id, MAX(c.first_name) AS first_name, MAX(c.last_name) AS last_name, 
-			i.billing_country AS country, SUM(i.total) AS total_spent,
-			ROW_NUMBER() OVER (PARTITION BY i.billing_country ORDER BY SUM(i.total) DESC) AS row_num
-			FROM customer AS c
-			INNER JOIN invoice AS i
-			ON c.customer_id = i.customer_id
-			GROUP BY c.customer_id, i.billing_country
+		SELECT c.customer_id, MAX(c.first_name) AS first_name, MAX(c.last_name) AS last_name, 
+		i.billing_country AS country, SUM(i.total) AS total_spent,
+		ROW_NUMBER() OVER (PARTITION BY i.billing_country ORDER BY SUM(i.total) DESC) AS row_num
+		FROM customer AS c
+		INNER JOIN invoice AS i
+		ON c.customer_id = i.customer_id
+		GROUP BY c.customer_id, i.billing_country
 )
 SELECT * FROM new
 WHERE row_num = 1
